506c8c500c12219d7f8553c3ddf5fae716604c6e
Conner Gleason <connereg@optonline.net>
2021-09-23 09:56:03 -0400

    commit tree 2a51dc794c073320d4f223eb785a0f34f5f7ea3a

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   # your code here
+  def initialize(attributes = nil)
+    attributes.each do |key, value| 
+        self.class.attr_accessor(key)  #DYNAMICALLY update attr_accessor // making Hardcoded attr_accessor pointless on line 7
+        self.send("#{key}=", value)
+    end
+  end 
+#
 end
